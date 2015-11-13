e9393a95cfda8a1f3f0967fd88c91ea908a9123a
Tom Leonardsson <tom.leo@hotmail.com>
2015-11-13 21:54:57 +0100

    fixed room and added classes

---
+++ core/src/com/tomnes/dd/gameScene/Enemy.java
@@ -0,0 +1,5 @@
+package com.tomnes.dd.gameScene;
+
+public class Enemy {
+
+}
--- core/src/com/tomnes/dd/level/Room.java
+++ core/src/com/tomnes/dd/level/Room.java
@@ -1,5 +1,38 @@
 package com.tomnes.dd.level;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.tomnes.dd.AssetManager;
+import com.tomnes.dd.framework.Animation;
+
 public class Room {
+	private float difficulty;
 	
+	private Animation sprite;
+	
+	private Vector2[] doors;
+	
+	public Room() {
+		sprite =  new Animation(AssetManager.getTexture("room1"));
+		sprite.setSize(9, 16);
+		
+		sprite.setPosition(-4.5f, -8);
+		
+		doors[0] = Vector2.Zero;
+		doors[1] = Vector2.Zero;
+		doors[2] = Vector2.Zero;
+		doors[3] = Vector2.Zero;
+	}
+	
+	public float getDifficulty() {
+		return difficulty;
+	}
+	
+	public void setDiffuclty(float difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	public void draw(SpriteBatch batch) {
+		sprite.draw(batch);
+	}
 }
