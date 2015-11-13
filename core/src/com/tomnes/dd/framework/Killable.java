e7f45344d8b9e285bb6bfef54573fcd87b555e13
Tom Leonardsson <tom.leo@hotmail.com>
2015-11-13 23:10:28 +0100

    Basics of enemy is done, added hitcount to killabl

--- core/src/com/tomnes/dd/framework/Killable.java
+++ core/src/com/tomnes/dd/framework/Killable.java
@@ -7,6 +7,9 @@ public abstract class Killable extends GameObject {
 	private float health;
 	private float maxHealth;
 	
+	private float hitTime;
+	private float maxHitTime;
+	
 	public Killable(Vector2 position, Vector2 size, Animation sprite) {
 		super(position, size, sprite);
 		// TODO Auto-generated constructor stub
@@ -15,12 +18,25 @@ public abstract class Killable extends GameObject {
 	public void update(float deltaTime) {
 		if(health <= 0) onDeath();
 		
+		if(hitTime > 0) {
+			hitTime += 0.1 * deltaTime;
+			if(hitTime >= maxHitTime) {
+				hitTime = 0;
+				getSprite().setColor(1, 1, 1, 1);
+			}
+		}
+		
 		super.update(deltaTime);
 	}
 	
 	// Can call on this to instantly kill object
 	public void onDeath() {
 		getScene().removeObject(this);
+	}
+	
+	public void onHit() {
+		hitTime = 0.1f;
+		getSprite().setColor(1, 0, 0, 1);
 	}
 	
 	public float getHealth() {
@@ -37,5 +53,21 @@ public abstract class Killable extends GameObject {
 	
 	public void setMaxHealth(float maxHealth) {
 		this.maxHealth = maxHealth;
+	}
+	
+	public float getHitTime() {
+		return hitTime;
+	}
+
+	public void setHitTime(float hitTime) {
+		this.hitTime = hitTime;
+	}
+
+	public float getMaxHitTime() {
+		return maxHitTime;
+	}
+
+	public void setMaxHitTime(float maxHitTime) {
+		this.maxHitTime = maxHitTime;
 	}
 }
