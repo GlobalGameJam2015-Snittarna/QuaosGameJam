bf1d3a079edb93ad8f21b7401865f6a89749baf1
Tom Leonardsson <tom.leo@hotmail.com>
2015-11-14 05:40:34 +0100

    power-up icons

--- android/assets/textures/data.gd
+++ android/assets/textures/data.gd
@@ -9,4 +9,12 @@ textures: {
 	joystick-center: 138, 0, 32, 32;
 	
 	bullet: 261, 129, 8, 8;
+	
+	laserIcon: 1, 649, 32, 32;
+	granadeIcon: 34, 649, 32, 32;
+	bulletIcon: 67, 649, 32, 32;
+	
+	oneShotIcon: 1, 682, 32, 32;
+	twoShotIcon: 34, 682, 32, 32;
+	threeShotIcon: 67, 682, 32, 32;
 }
